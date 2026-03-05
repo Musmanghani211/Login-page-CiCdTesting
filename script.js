6f3e1532c1454e1202ef35a5588a14c5c781ff16
M usman ghani <musmanghani211@gmail.com>
2026-03-05 12:53:11 +0500

    script error change

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ loginForm.addEventListener('submit', function(e) {
     alert('Login successful!');
     errorMsg.textContent = '';
   } else if(username === 'locked_out_user') {
-    errorMsg.textContent = 'Epic sadface: Sorry, this user has been locked out.';
+    errorMsg.textContent = 'Sorry, this user has been locked out.';
   } else {
-    errorMsg.textContent = 'Epic sadface: Username or password is incorrect.';
+    errorMsg.textContent = 'Sorry, your username or password is incorrect.';
   }
 });
